62b22206acc0a3fe184c045afae4b3e5202c53f6
Mr Tinker <example@example.com>
2026-06-04 13:28:48 +0300

    shorten 16bit presentation

--- lab_05/variant.asm
+++ lab_05/variant.asm
@@ -1,8 +1,14 @@
+%include "constants.inc"
+%include "../chars.inc"
+%include "../linux_sys.inc"
+
 global print_exceeding_degree
-
-
 
 section .text
 print_exceeding_degree:
+    ; the number is in rdi
+    bsr rax, rdi
+    shl rax, 1
+    
     ret
     
